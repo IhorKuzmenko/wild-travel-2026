0d600d6de91d3c60d624ac1e1298061581e398ce
Ihor Kuzmenko <nameless1306@gmail.com>
2026-07-12 23:23:19 +0300

    resolve errors

--- lib/api/api.ts
+++ lib/api/api.ts
@@ -1,7 +1,8 @@
 import axios from 'axios';
 
-export const nextServer = axios.create({
-  baseURL: process.env.NEXT_PUBLIC_API_URL || 'http://localhost:3000',
-  withCredentials: false,
-});
+export const nextServer = axios.create();
 
+// export const nextServer = axios.create({
+//   baseURL: process.env.NEXT_PUBLIC_API_URL || 'http://localhost:3000',
+//   withCredentials: false,
+// });
